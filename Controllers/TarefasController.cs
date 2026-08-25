533abcef377dd203c7dec83dce3b28fb5e1f4c45
wcc_devs <wcc_devs@WCC-LAB36-61329>
2026-08-25 20:57:00 -0300

    Add project files.

---
+++ Controllers/TarefasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaTarefa.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace ListaTarefa.API.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Status { get; set; }
+        public DateTime dataCriacao { get; set; } = DateTime.Now;
+
+    }
+}
